b1a4f171775ea2f7a3746a5ae94cfc704c94d187
Benizachicazadasmok <iosif.goga01@e-uvt.ro>
2022-04-30 01:32:06 +0300

    Added a menu and win scene.

---
+++ Unicorn Connect4/Assets/Scripts/SceneChanger.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneChanger : MonoBehaviour
+{
+    public void Start_game()
+    {
+        SceneManager.LoadScene("Game");
+    }
+
+    public void Start_game_de_la_tastatura()
+    {
+
+        if (Input.GetKeyDown(KeyCode.Alpha9))
+        {
+            SceneManager.LoadScene("Game");
+        }
+    }
+
+    public void Quit_game()
+    {
+        Debug.Log("exitgame");
+        Application.Quit();
+    }
+
+    public void Quit_game_de_la_tastatura()
+    {
+
+        if (Input.GetKeyDown(KeyCode.Alpha0))
+        {
+            Debug.Log("exitgame");
+            Application.Quit();
+        }
+    }
+
+
+    void Update()
+    {
+        Start_game_de_la_tastatura();
+        Quit_game_de_la_tastatura();
+    }
+}
