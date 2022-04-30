11a51d21ef12b5786fb59ebf98b8ab2fb71b9819
David Vuescu <davidvuescu@gmail.com>
2022-04-30 23:26:22 +0300

    Complited API game integration Merged all scripts into gameController.cs Finished speller integration
    Co-authored-by: DariusBad <dariusbadea04@gmail.com>
    Co-authored-by: Benizachicazadasmok <iosif.goga01@e-uvt.ro>
    Co-authored-by: DavidVuescu <davidvuescu@gmail.com>

--- Unicorn Connect4/Assets/Scripts/SceneChanger.cs
+++ Unicorn Connect4/Assets/Scripts/SceneChanger.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class SceneChanger : MonoBehaviour
+public class sceneChanger : MonoBehaviour
 {
     public void Start_game()
     {
