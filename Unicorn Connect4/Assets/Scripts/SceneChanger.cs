7a49995682713c1d612113686d5e4078440cdc4e
Benizachicazadasmok <iosif.goga01@e-uvt.ro>
2022-05-01 04:58:52 +0300

    Trying to make the Bci work in menus
    Former-commit-id: ea188da902285c86d5b03a6d1b5852de1008cb4a

--- Unicorn Connect4/Assets/Scripts/SceneChanger.cs
+++ Unicorn Connect4/Assets/Scripts/SceneChanger.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
-
+using GameControlls;
 public class SceneChanger : MonoBehaviour
 {
     public void Start_game()
