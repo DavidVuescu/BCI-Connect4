c30fc6be2e8634c45405776deee458af08461c82
David Vuescu <davidvuescu@gmail.com>
2022-05-01 12:37:26 +0300

    Added bootleg game AI We also sold our sanity for a clean build We are free
    Co-authored-by: Benizachicazadasmok <iosif.goga01@e-uvt.ro>
    
    Former-commit-id: cdfa38d9665dc3ec7ef0e492ff7a51eb02a40a24

---
+++ Unicorn Connect4/Assets/Scripts/Settings.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace gameSettings
+{
+    public class Settings : MonoBehaviour
+    {
+        public static bool aiActive = true;
+    }
+}
